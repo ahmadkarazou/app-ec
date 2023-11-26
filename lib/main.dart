87dfdcf8907b30d28c4e68c60159e33a71750983
haneenah <148560961+HaneenAbumazrou@users.noreply.github.com>
2023-11-26 15:42:43 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
+import 'Profile.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MaterialApp(
+debugShowCheckedModeBanner: false,
+    home: Profile (),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-
-        useMaterial3: true,
-      ),
-      home: Home(),
-    );
-  }
-}
 
 
 
